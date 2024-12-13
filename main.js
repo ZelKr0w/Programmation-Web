da17fb9d57c5a956e6be9036d456abd7206d934c
Melkout <minouchkamk@gmail.com>
2024-12-13 14:43:38 +0100

    chgmnt creation page

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-const body = document.querySelector('body')
-
 function CreatePage() {
     CreateHeader() ;
     CreateFooter() ;
 }
 
 function CreateHeader() {
-    const main = document.getElementById("main") ;
-    const body = document.getElementById("body") ;
+    const main = document.querySelector("main") ;
+    const body = document.querySelector("body") ;
     const txtheader = '<header><h1>Typing Challenge</h1><i class="fa-regular fa-keyboard"></i><nav class="nav"><a href="./index.html"><i class="fa-solid fa-house"></i></a><a href="./Selection/Select_text.html">Texte</a><a href="./Selection/Select_mot_h.html">Mot au Hasard</a><a href="./Selection/Select_phrase_h.html">Phrase au Hasard</a></nav></header>' ;
     const header = document.createElement("header") ;
     header.innerHTML = txtheader ;
@@ -15,7 +13,7 @@ function CreateHeader() {
 }
 function CreateFooter() {
     /** Create footer for all pages */
-    const body = document.getElementById("body") ;
+    const body = document.querySelector("body") ;
     const txtfooter = "<footer> <p>Fait par</p><ul><li>Axel BOUTIE</li><li>Melwan KOUTAINE</li><li>Lasse KUHNT</li><li>Lorcan JOHNSON</li></ul></footer>" ;
     const footer = document.createElement("footer") ;
     footer.innerHTML = txtfooter ;
