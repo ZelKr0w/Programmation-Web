3736ea7c5ba1fd580e0abe89c85a22b5fea6a590
Axel <Zo0mMy21@gmail.com>
2025-01-05 10:28:24 +0100

    Remise en forme et suppression des autres catégories

--- Selection/select.js
+++
@@ -1,7 +0,0 @@
-const tps = document.getElementsByClassName("tps");
-
-let label = document.createElement("label");
-label.for = "tps-select";
-label.value = "Choose your time";
-
-tps.appendChild = label;
